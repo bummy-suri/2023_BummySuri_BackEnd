5f1a05740df5e75a3175b82c970f85f05a4e2b69
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-08-25 14:22:13 +0900

    fix minor error

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,12 +1,12 @@
 import { PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientRustPanicError, PrismaClientUnknownRequestError } from "@prisma/client/runtime/library";
 
 
-const isErrorOfType = (error: Error, types: Function[]) : boolean => {
+const isErrorOfType = (error: any, types: Function[]) : boolean => {
     return types.some(type => error instanceof type)
 };
 
 
-export const isErrorOfPrisma = (error: Error) : boolean => {
+export const isErrorOfPrisma = (error: any) : boolean => {
     return isErrorOfType(error, [PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientRustPanicError, PrismaClientUnknownRequestError])
 }
 
