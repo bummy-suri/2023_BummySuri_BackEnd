f87d65092a7add3b2c727ce263656e7841699c1a
Yeaeun411 <yeaeun411@naver.com>
2023-08-25 19:25:30 +0900

    feat: userRepository & service

--- src/repositories/users.ts
+++ src/repositories/users.ts
@@ -4,18 +4,44 @@ import { PrismaError } from "../utils/errors";
 
 const prisma = new PrismaClient();
 
-
-
-export const createUser = async (user: UserType) : Promise<Number> => {
-
+export const createUser = async (user: UserType): Promise<number> => {
     return prisma.user.create({
         data: {
-            klaytn_address: user.userCardAddress,
-            totalPoint: 1000,
+            userCardAddress: user.userCardAddress,
+            name: user.name,
+            univ: user.univ,
+            phone: user.phone,
+            studentId: user.studentId,
+            NFT_image: user.NFT_image,
+            totalPoint: user.totalPoint
         },
     }).then((result) => {
-        return result.id
+        return result.id;
     }).catch((e) => {
-        throw new PrismaError(e?.message)
+        throw new PrismaError(e?.message);
+    })
+}
+
+export const getUser = async (user: UserType): Promise<UserType | null> => {
+    return prisma.user.findUnique({
+        where: {
+            id: user.id
+        }
+    }).then((result) => {
+        return result ? result : null;
+    }).catch((e) => {
+        throw new PrismaError(e?.message);
+    })
+}
+
+export const deleteUser = async (user: UserType): Promise<number> => {
+    return prisma.user.delete({
+        where: {
+            id: user.id
+        }
+    }).then((result) => {
+        return result.id;
+    }).catch((e) => {
+        throw new PrismaError(e?.message);
     })
 }
