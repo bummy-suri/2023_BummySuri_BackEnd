ad85fe1c91738e4a45cac258ee5821dc2fb8abe1
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-08-28 12:41:29 +0900

    create grandUser apis

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,3 +1,4 @@
+import { handleApp2AppResultStateAPIs } from "../apis";
 import { TokenType, UserType } from "../models/sample";
 import { createUser, getUser, deleteUser } from "../repositories/users";
 import { generateToken } from "./auth";
@@ -20,6 +21,25 @@ export const mintUser = async (user: UserType) : Promise<TokenType> => {
 
     return {
         access: token,
+    }
+}
+
+export const grantUser = async (requestKey: string) : Promise<TokenType> => {
+
+    try {
+        const address = await handleApp2AppResultStateAPIs(requestKey)
+
+        // regard user exists at userid = 1
+        const userid = 1
+
+        const token = generateToken(userid.toString())
+
+        return {
+            access: token,
+        }
+
+    } catch (e) {
+        throw e;
     }
 }
 
