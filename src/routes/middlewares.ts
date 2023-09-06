f179771eb95606e6bc113087637cf54b7c111851
Yeaeun411 <yeaeun411@naver.com>
2023-09-06 15:50:23 +0900

    fix middleware adn schema

--- src/routes/middlewares.ts
+++ src/routes/middlewares.ts
@@ -14,37 +14,38 @@ declare global {
 
 
 export const authenticateMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
-        return res.sendStatus(401);
+        return res.status(401).json({ message: 'Authorization token is missing.' });
     }
 
     const { ok, userid } = parseToken(token);
 
     if (!ok) {
-        return res.sendStatus(403);
+        return res.status(403).json({ message: 'Invalid or expired authorization token.' });
     }
 
-    req.userid = userid;
+    req.userid = userid;  // req 객체에 userid를 저장하여 후속 미들웨어나 라우터에서 사용할 수 있게 함
     next();
 };
 
 
 export const mintAuthenticateMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
-        return res.sendStatus(401);
+        return res.status(401).json({ message: 'Authorization token is missing.' });
     }
 
     const { ok, userid, isMinted } = parseToken(token);
 
     if (!ok) {
-        return res.sendStatus(403);
+        return res.status(403).json({ message: 'Invalid or expired authorization token.' });
     }
+
+    // 여기서 userid와 isMinted 등의 값을 다음 미들웨어나 라우터 핸들러에서 사용할 수 있도록 req 객체에 추가할 수 있습니다.
+    // 예: req.userid = userid;
 
     next();
 };
