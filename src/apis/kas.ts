d561ebe758a25bb03b219a26332d8c4c59f60df6
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-07 00:47:40 +0900

    add description

--- src/apis/kas.ts
+++ src/apis/kas.ts
@@ -1,9 +1,15 @@
 import { NFTType } from "../models/sample";
 import sleep from "../utils/sleep";
 
+// here are where the function external module calls exists
 
-
+// mock function
 export const acquireNFT = async ({team, tokenId, cardAddress}: NFTType) => {
+    await sleep(5000);
+}
+
+// real function
+export const acquireNFTREAL = async ({team, tokenId, cardAddress}: NFTType) => {
     await sleep(5000);
 }
 
