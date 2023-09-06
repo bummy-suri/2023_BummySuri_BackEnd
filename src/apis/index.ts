e9c00fbbf912a49235ecd225c470191e7188cea0
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-06 23:53:32 +0900

    export aquireNFT

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
-
-
 import { handleApp2AppResultState } from "../apis/klip"
+import { acquireNFT } from "../apis/kas"
 
 export {
-    handleApp2AppResultState as handleApp2AppResultStateAPIs
+    handleApp2AppResultState as handleApp2AppResultStateAPIs,
+    acquireNFT as acquireNFTAPIs
 };
