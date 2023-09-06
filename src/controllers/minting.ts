d64be362b12fda805ffff6f97c42a0c0d725fdd3
Yeaeun411 <yeaeun411@naver.com>
2023-09-06 16:08:10 +0900

    mockingData

--- src/controllers/minting.ts
+++ src/controllers/minting.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { ZodError, z } from "zod";
 import { PrismaError } from "../utils/errors";
 import { AxiosError } from "axios";
-import { getNFTCountService, mintingService } from "../services"; // 추후에 서비스 로직을 추가할 경우
+import { getNFTCountService, mintingService, getMetaDataService } from "../services"; // 추후에 서비스 로직을 추가할 경우
 
 export const teamTypeSchema = z.object({
     teamType: z.enum(["KOREA","YONSEI"])
@@ -18,7 +18,8 @@ export const getNFTCount = async (req: Request, res: Response) => {
     try {
         const team = teamTypeSchema.parse({ teamType: req.params.teamType });
         const NFTCountdData = await getNFTCountService(team.teamType);
-        res.json(NFTCountdData as getNFTCountResponse);
+        res.json({count: 2301})
+        //res.json(NFTCountdData as getNFTCountResponse);
     } catch (error) {
         if (error instanceof ZodError) {
             res.status(400).send(error.message);
@@ -72,3 +73,29 @@ export const minting = async (req: Request, res: Response) => {
         }      
     }
 };        
+
+export const getMetaData = async (req: Request, res: Response) => {
+    try{
+        const contractAddress = req.params.contractAddress;
+        const tokenId = req.params.tokenId;
+        const metadata = await getMetaDataService(contractAddress, tokenId);
+        return res.json(metadata);
+    } catch (error) {
+        if (error instanceof ZodError) {
+            res.status(400).send(error.message);
+            return
+        }
+        if (error instanceof PrismaError) {
+            res.status(503).send(error.message);
+            return
+        }
+        if (error instanceof AxiosError) {
+            res.status(502).send(error.message);
+            return
+        }
+        if (error instanceof Error) {
+            res.status(500).send(error.message);
+            return
+        }      
+    }
+};
