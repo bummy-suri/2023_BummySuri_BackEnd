125cfbc01147e3a1fb3e70d64519bddd4bb34840
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-07 00:00:35 +0900

    fix error

--- kas.ts
+++ kas.ts
@@ -1,3 +1,31 @@
-import main from "./src/apis/kas";
+require('dotenv').config();
 
-main().then(err => console.log(err)).catch(err => console.error(err));
+import { getContractList } from "./src/apis/atom"
+
+// KASKLL5HLB44QZCE0KZPWVQZ
+
+// 5PB7tu1i_nkSnhFGQIjziymsqURyDMA8peOtSFRC
+
+// Basic S0FTS0xMNUhMQjQ0UVpDRTBLWlBXVlFaOjVQQjd0dTFpX25rU25oRkdRSWp6aXltc3FVUnlETUE4cGVPdFNGUkM=
+
+getContractList()
+.then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+})
+
+
+
+    /*
+const main = async () => {
+
+    try {
+        console.log(await getContractList())
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
+main();
+*/
