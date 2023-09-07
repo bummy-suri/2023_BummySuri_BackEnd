5896fb0c643bb67f40b10f62d3bb43073ecce69a
wonj1012 <jjaa1012@gmail.com>
2023-09-08 00:15:55 +0900

    feat: Add parity verification for team type

--- src/services/minting.ts
+++ src/services/minting.ts
@@ -1,114 +1,144 @@
 import { TeamType } from "@prisma/client";
-import { MetaDataType, NFTCountType,  } from "../models/sample";
-import { getUserPersistance, getNFTCountPersistance , updateNFTCountPersistance, updateUserPersistance, getMetaDataPersistance, getAvailableTokenIdPersistance, createIssuedRecordPersistance} from "../repositories";
+import { MetaDataType, NFTCountType } from "../models/sample";
+import {
+  getUserPersistance,
+  getNFTCountPersistance,
+  updateNFTCountPersistance,
+  updateUserPersistance,
+  getMetaDataPersistance,
+  getAvailableTokenIdPersistance,
+  createIssuedRecordPersistance,
+} from "../repositories";
 import { generateToken } from "./auth";
 import { acquireNFT } from "../apis/kas";
 import { BUMMY_CONTRACT, SURI_CONTRACT } from "../apis/init";
 
-export const getNFTCountData = async (team: TeamType): Promise<NFTCountType> => {
-    try {
-        const NFTCountData = await getNFTCountPersistance(team);
-        
-        if (!NFTCountData) {
-            throw new Error(`NFTCountData not found for team ${team}`);
-        }
-        
-        return NFTCountData;
-    } catch (e) {
-        throw e;
+export const getNFTCountData = async (
+  team: TeamType
+): Promise<NFTCountType> => {
+  try {
+    const NFTCountData = await getNFTCountPersistance(team);
+
+    if (!NFTCountData) {
+      throw new Error(`NFTCountData not found for team ${team}`);
     }
+
+    return NFTCountData;
+  } catch (e) {
+    throw e;
+  }
 };
 
+export const minting = async (
+  userid: number,
+  team: TeamType
+): Promise<number | boolean> => {
+  try {
+    let contractAddr = BUMMY_CONTRACT;
 
-
-export const minting = async (userid: number ,team: TeamType): Promise<number | boolean> => {
-    try {
-        let contractAddr = BUMMY_CONTRACT;
-
-        if (team === "KOREA"){
-            contractAddr = BUMMY_CONTRACT;
-        }else if(team === "YONSEI"){
-            contractAddr = SURI_CONTRACT; 
-        }
-
-        if (!contractAddr) {
-            throw new Error(`contractAddr not found for team ${team}`);
-        }
-        
-        //check the count
-        const countData = await getNFTCountPersistance(team);
-        if (!countData) {
-            throw new Error(`NFTCountData not found for team ${team}`);
-        }else{
-            if(countData.count >= 5000){
-                throw new Error(`NFTCountData is over 5000 for team ${team}`);
-            }
-
-            let userData = await getUserPersistance(userid);
-            if (!userData) {
-                throw new Error(`userCardAddress not found for user ${userid}`);
-            }
-
-            const userCardAddress = userData.userCardAddress;
-
-            //isMinted check
-            if (userData.isMinted === true) {
-                return false;
-            }
-
-            //get AvailableTokenId
-            const userTokenId = await getAvailableTokenIdPersistance(contractAddr);
-
-            if (!userTokenId) {
-                throw new Error(`AvailableTokenId not found`);
-            }
-
-            //minting
-            await acquireNFT({team, tokenId: userTokenId, cardAddress: userCardAddress});
-
-            //issuedRecord
-            await createIssuedRecordPersistance(userid, userTokenId, contractAddr);
-
-            //update the user isMinted to true
-            userData = await updateUserPersistance(userid, team, true);
-
-            //update the NFTcount
-            const updatedCountData= await updateNFTCountPersistance(team);
-            
-             //generate accessToken
-            const token = parseInt(generateToken(userid, true));
-            return token;
-        }
-        
-    } catch (e) {
-        throw e;
-    }
-}
-
-
-export const getMetaData = async (contractAddr: string, tokenId: number): Promise<MetaDataType> => {
-    const tokenData = await getMetaDataPersistance(contractAddr, tokenId);
-
-    const attributes = tokenData.attributes.map(attr => ({
-        trait_type: attr.key,
-        value: attr.value
-    }));
-
-    let group_name = "";
-    let group_icon = "";
-
-    if (contractAddr === BUMMY_CONTRACT){
-        group_name = "버미";
-        group_icon = "https://static.bummysuri.com/asset/bummy_badge.png";
-    }else if(contractAddr === SURI_CONTRACT){
-        group_name = "수리";
-        group_icon = "https://static.bummysuri.com/asset/suri_badge.png";
+    if (team === "KOREA") {
+      contractAddr = BUMMY_CONTRACT;
+    } else if (team === "YONSEI") {
+      contractAddr = SURI_CONTRACT;
     }
 
-    return {
-        "image": "https://static.bummysuri.com/asset/"+contractAddr+"/"+tokenData.image,
-        "group_name": group_name,
-        "group_icon": group_icon,
-        "attributes": attributes
-    };
+    if (!contractAddr) {
+      throw new Error(`contractAddr not found for team ${team}`);
+    }
+
+    //check the count
+    const countData = await getNFTCountPersistance(team);
+    if (!countData) {
+      throw new Error(`NFTCountData not found for team ${team}`);
+    } else {
+      if (countData.count >= 5000) {
+        throw new Error(`NFTCountData is over 5000 for team ${team}`);
+      }
+
+      let userData = await getUserPersistance(userid);
+      if (!userData) {
+        throw new Error(`userCardAddress not found for user ${userid}`);
+      }
+
+      const userCardAddress = userData.userCardAddress;
+
+      //isMinted check
+      if (userData.isMinted === true) {
+        return false;
+      }
+
+      //get AvailableTokenId
+      const userTokenId = await getAvailableTokenIdPersistance(contractAddr);
+
+      if (!userTokenId) {
+        throw new Error(`AvailableTokenId not found`);
+      }
+
+      if (team === "KOREA") {
+        if (userTokenId % 2 !== 0) {
+          throw new Error(`AvailableTokenId is not even number`);
+        }
+      } else if (team === "YONSEI") {
+        if (userTokenId % 2 !== 1) {
+          throw new Error(`AvailableTokenId is not odd number`);
+        }
+      }
+
+      //minting
+      await acquireNFT({
+        team,
+        tokenId: userTokenId,
+        cardAddress: userCardAddress,
+      });
+
+      //issuedRecord
+      await createIssuedRecordPersistance(userid, userTokenId, contractAddr);
+
+      //update the user isMinted to true
+      userData = await updateUserPersistance(userid, team, true);
+
+      //update the NFTcount
+      const updatedCountData = await updateNFTCountPersistance(team);
+
+      //generate accessToken
+      const token = parseInt(generateToken(userid, true));
+      return token;
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const getMetaData = async (
+  contractAddr: string,
+  tokenId: number
+): Promise<MetaDataType> => {
+  const tokenData = await getMetaDataPersistance(contractAddr, tokenId);
+
+  const attributes = tokenData.attributes.map((attr) => ({
+    trait_type: attr.key,
+    value: attr.value,
+  }));
+
+  let group_name = "";
+  let group_icon = "";
+
+  if (contractAddr === BUMMY_CONTRACT) {
+    group_name = "버미";
+    group_icon = "https://static.bummysuri.com/asset/bummy_badge.png";
+  } else if (contractAddr === SURI_CONTRACT) {
+    group_name = "수리";
+    group_icon = "https://static.bummysuri.com/asset/suri_badge.png";
+  }
+
+  return {
+    image:
+      "https://static.bummysuri.com/asset/" +
+      contractAddr +
+      "/" +
+      tokenData.image,
+    group_name: group_name,
+    group_icon: group_icon,
+    attributes: attributes,
+  };
 };
